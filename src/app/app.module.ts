7d1527bae86cf28f05d2817775da93b37880fb84
estherkoko <estherkolade@gmail.com>
2018-08-24 22:10:32 -0400

    updated src files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { WineComponent } from './wine/wine.component';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
+//import { Ng2PaginationModule } from 'ng2-pagination';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { FilterPipe} from './filter.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WineComponent
+    WineComponent,
+    FilterPipe
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule,
+    //Ng2PaginationModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/wine/wine.component.ts
+++ src/app/wine/wine.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import  { HttpClient } from  '@angular/common/http';
 import { WineService } from '../shared/wine.service';
 import { NgForm } from '../../../node_modules/@angular/forms';
+import { Wine } from '../shared/wine.model';
+import { FilterPipe} from '../filter.pipe';
+
+//import { toast } from '../../../../../../../node_modules/angular2-materialize';
+
 declare var M: any;
 @Component({
   selector: 'app-wine',
@@ -10,11 +15,17 @@ declare var M: any;
   providers :[WineService]
 })
 export class WineComponent implements OnInit {
-
+  //newwines: any;
+ p:number = 1;
+ searchText :any;
   //add constructor parameter
-  constructor(public wineService: WineService) {}
+  constructor(public wineService: WineService, private http: HttpClient ) {}
 
   ngOnInit() {
+    /*this.http.get('http://localhost:3000/wine').subscribe(data => {
+      this.newwines = data;
+        });*/
+    this.refreshWineList();
     this.resetForm();
   }
 
@@ -40,11 +51,47 @@ export class WineComponent implements OnInit {
     }
  //submit form to enter data into the database
  onSubmit(form:NgForm){
+   if(form.value._id==""){
   this.wineService.postWine(form.value).subscribe((res)=>{
     this.resetForm(form);
+    this.refreshWineList();
     M.toast({html : 'Saved successfully', classes:'rounded'});
   });
 }
+//update the data for the current id
+else{
+  this.wineService.putWine(form.value).subscribe((res)=>{
+    this.resetForm(form);
+    this.refreshWineList();
+    M.toast({html : 'Updated successfully', classes:'rounded'});
+  });
+}
+}
+
+//add array collection to the response
+refreshWineList(){
+  this.wineService.getWineList().subscribe((res)=>{
+    this.wineService.wines=res as Wine[];
+});
+}
+
+//function to allow edit of data and update the db
+onEdit(w: Wine){
+  this.wineService.selectedWine=w;
+}
+//function that returns an observer 
+onDelete(_id: string, form: NgForm){
+  if(confirm('Are you sure you want to delete this wine?')==true){
+    this.wineService.deleteWine(_id).subscribe((res) =>
+    {
+      this.refreshWineList();
+      this.resetForm(form);
+      M.toast({html : 'Deleted successfully', classes:'rounded'});
+    });
   }
+}
+
+
+}
 
 
